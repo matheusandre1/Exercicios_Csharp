d3dbd2661ad44c03f1e70416bb2f228b906b4e10
Matheus Andre <matheusandr2@gmail.com>
2023-04-28 15:06:54 -0300

    Exercicio03 Modif

--- Exercicios/Exercicio02/Program.cs
+++ Exercicios/Exercicio02/Program.cs
@@ -4,9 +4,10 @@ using Exercicio02;
 
 CotaDolar converter = new CotaDolar();
 
+Console.Write("Digite O Dolar a ser convertido:" );
 converter.Dolar = Double.Parse(Console.ReadLine());
 
 converter.Cotacao = 5.32;
 double resultado =converter.ConverterPraReal();
 
-Console.WriteLine($"O Valor Em Dolar de {converter.Dolar} é de: {resultado}");
+Console.WriteLine($"O Valor Em Dolar de {converter.Dolar} é de: {resultado.ToString("F2",CultureInfo.InvariantCulture)}");
